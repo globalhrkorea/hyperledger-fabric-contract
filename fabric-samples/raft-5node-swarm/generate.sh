e72418c9a9853a84ad4373b989c44f5403a84fa4
ljwoon94 <ljwoon94@gmail.com>
2021-09-23 01:22:33 +0000

    mychannel로 변경

--- fabric-samples/raft-5node-swarm/generate.sh
+++ fabric-samples/raft-5node-swarm/generate.sh
@@ -6,22 +6,5 @@ mkdir channel-artifacts
 
 ../bin/configtxgen -profile SampleMultiNodeEtcdRaft -outputBlock ./channel-artifacts/genesis.block -channelID system-channel
 
-../bin/configtxgen -profile Org1Channel -outputCreateChannelTx ./channel-artifacts/org1channel.tx -channelID org1channel
+../bin/configtxgen -profile MyChannel -outputCreateChannelTx ./channel-artifacts/mychannel.tx -channelID mychannel
 
-../bin/configtxgen -profile Org1Channel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID org1channel -asOrg Org1MSP
-
-../bin/configtxgen -profile Org2Channel -outputCreateChannelTx ./channel-artifacts/org2channel.tx -channelID org2channel
-
-../bin/configtxgen -profile Org2Channel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID org2channel -asOrg Org2MSP
-
-../bin/configtxgen -profile Org3Channel -outputCreateChannelTx ./channel-artifacts/org3channel.tx -channelID org3channel
-
-../bin/configtxgen -profile Org3Channel -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors.tx -channelID org3channel -asOrg Org3MSP
-
-../bin/configtxgen -profile Org4Channel -outputCreateChannelTx ./channel-artifacts/org4channel.tx -channelID org4channel
-
-../bin/configtxgen -profile Org4Channel -outputAnchorPeersUpdate ./channel-artifacts/Org4MSPanchors.tx -channelID org4channel -asOrg Org4MSP
-
-../bin/configtxgen -profile Org5Channel -outputCreateChannelTx ./channel-artifacts/org5channel.tx -channelID org5channel
-
-../bin/configtxgen -profile Org5Channel -outputAnchorPeersUpdate ./channel-artifacts/Org5MSPanchors.tx -channelID org5channel -asOrg Org5MSP
